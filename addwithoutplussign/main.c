680ad9ef3a5abe6687551f6520850fc25e9273b2
Pranjal Ghimire <ghimirepranjal@protonmail.com>
2020-12-26 15:32:51 -0600

    finished add without + sign

---
+++ addwithoutplussign/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    printf("Please enter two numbers\n");
+    scanf("%d %d",&a,&b);
+
+    if (a>0){
+    while(a!=0){
+        a--;
+        b++;
+
+    }
+
+    }
+
+    else{
+
+    while(a!=0){
+        a++;
+        b--;
+
+    }
+
+
+    }
+
+    printf("the sum is %d",b);
+
+
+    return 0;
+}
